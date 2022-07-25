834f8b03d4f45d41714bf6fbd04cc270e416d8f7
김남준 <namjun88@gmail.com>
2022-07-25 21:22:34 +0900

    first

---
+++ cheater_killer-2/src/main/java/com/example/demo/CheaterKiller2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheaterKiller2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheaterKiller2Application.class, args);
+	}
+
+}
